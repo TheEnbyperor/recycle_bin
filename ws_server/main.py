2ed78d3ee116f446c7de3102a0556ab497d211a4
Q Misell <q@misell.cymru>
2019-05-13 09:40:14 +0100

    Improved barcode algorithm with adaptive threshold

--- ws_server/main.py
+++ ws_server/main.py
@@ -35,7 +35,7 @@ def service_signal(signum, _):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
     signal.signal(signal.SIGTERM, service_signal)
     signal.signal(signal.SIGINT, service_signal)
--- ws_server/scan.py
+++ ws_server/scan.py
@@ -1,24 +1,23 @@
 import cv2
+import copy
+import time
 import math
 import queue
-import collections
 import zbar
-import imutils
 import threading
 import logging
-import numpy as np
 
 
 class BarcodeScanner:
     def __init__(self, cam, exit_event: threading.Event):
         self._cam = cam
         self._thread_exit = exit_event
-        self._scan_queue = queue.LifoQueue(maxsize=10)
+        self._scan_queue = queue.LifoQueue(maxsize=2)
         self._code_queues = set()
         self._img_queues = set()
         self._scanner = zbar.Scanner()
         self._detect_t = threading.Thread(target=self.fill_cam_queue, daemon=True)
-        self._scan_t = threading.Thread(target=self.scan_thread, daemon=True)
+        self._scan_t = threading.Thread(target=self.scan_tf, daemon=True)
         self._logger = logging.getLogger(__name__)
 
     def add_code_queue(self, q):
@@ -60,95 +59,36 @@ class BarcodeScanner:
 
         return newWidth, newHeight
 
-    def scan_thread(self):
+    def scan_tf(self):
         while not self._thread_exit.is_set():
-            if not self._scan_queue.empty():
-                frame = self._scan_queue.get()
-                results = self._scanner.scan(frame)
-                results = sorted(results, key=lambda r: r.quality, reverse=True)
-                for result in results:
+            try:
+                frame = self._scan_queue.get_nowait()
+            except queue.Empty:
+                time.sleep(0.1)
+                continue
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.medianBlur(gray, 5)
+            thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 1.5)
+            results = self._scanner.scan(thresh)
+            results = sorted(results, key=lambda r: r.quality, reverse=True)
+            for result in results:
+                try:
+                    data = result.data.decode()
+                except UnicodeDecodeError:
+                    self._logger.warning(f"Unable to debug barcode with raw data {result.data!r}")
+                    continue
+                self._logger.info(f"Scanned barcode with data {data!r}")
+                for q in copy.copy(self._code_queues):
                     try:
-                        data = result.data.decode()
-                    except UnicodeDecodeError:
-                        self._logger.warning(f"Unable to debug barcode with raw data {result.data!r}")
-                        continue
-                    self._logger.info(f"Scanned barcode with data {data!r}")
-                    for q in self._code_queues:
-                        try:
-                            q.put_nowait(data)
-                        except queue.Full:
-                            pass
-
-    def _rankContours(self, frame, c):
-        ddepth = cv2.cv.CV_32F if imutils.is_cv2() else cv2.CV_32F
-        rect = cv2.minAreaRect(c)
-        rect = ((rect[0][0], rect[0][1]), (rect[1][0] + 30, rect[1][1] + 30), rect[2])
-        if rect[1][0] < rect[1][1]:
-            rect = (rect[0], (rect[1][1], rect[1][0]), rect[2] + 90)
-
-        rotMatrix = cv2.getRotationMatrix2D(rect[0], rect[2], 1.0)
-        rotWidth, rotHeight = self._newBouncingBox((frame.shape[1], frame.shape[0]), rect[2])
-        rotImage = cv2.warpAffine(frame, rotMatrix, (rotWidth, rotHeight), flags=cv2.INTER_LINEAR)
-
-        boxWidth, boxHeight = np.int0(rect[1])
-        boxOrigX, boxOrigY = np.int0((rect[0][0] - rect[1][0] / 2.0, rect[0][1] - rect[1][1] / 2.0))
-        rotImage = rotImage[boxOrigY:boxOrigY + boxHeight, boxOrigX:boxOrigX + boxWidth]
-
-        if rotImage.size != 0:
-            gradX = cv2.Sobel(rotImage, ddepth=ddepth, dx=1, dy=0, ksize=-1)
-            gradY = cv2.Sobel(rotImage, ddepth=ddepth, dx=0, dy=1, ksize=-1)
-            gradient = cv2.subtract(gradX, gradY)
-            gradient = cv2.convertScaleAbs(gradient)
-            blurred = cv2.blur(gradient, (9, 9))
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
-            closed = cv2.morphologyEx(blurred, cv2.MORPH_CLOSE, kernel)
-            closed = cv2.erode(closed, None, iterations=4)
-            closed = cv2.dilate(closed, None, iterations=4)
-
-            return rotImage, rect, np.mean(closed)
-        else:
-            return rotImage, rect, 0
+                        q.put_nowait(data)
+                    except queue.Full:
+                        pass
+                break
+        self._logger.info("Barcode decoding thread exiting")
 
     def fill_cam_queue(self):
         i = 0
-        cur_contours = []
-        find_barcode_queue = collections.deque(maxlen=3)
-        contour_queue = collections.deque(maxlen=3)
-
-        def find_barcode_f():
-            while not self._thread_exit.is_set():
-                try:
-                    frame = find_barcode_queue.pop()
-                except IndexError:
-                    continue
-
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                (_, thresh) = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-                cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                cnts = imutils.grab_contours(cnts)
-                cs = sorted(cnts, key=cv2.contourArea, reverse=True)
-                cs = map(lambda c: self._rankContours(gray, c), cs[0:10])
-                cs = sorted(cs, key=lambda c: c[2], reverse=True)
-                contours = []
-                for i in range(min(3, len(cs))):
-                    c = cs[i]
-                    rotImage = c[0]
-                    rect = c[1]
-
-                    box = cv2.cv.boxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
-                    box = np.int0(box)
-                    contours.append(box)
-
-                    if rotImage.size != 0:
-                        self._logger.debug("Detected possible barcode")
-                        try:
-                            self._scan_queue.put_nowait(rotImage)
-                        except queue.Full:
-                            pass
-                contour_queue.append(contours)
-
-        find_barcode_t = threading.Thread(target=find_barcode_f, daemon=True)
-        find_barcode_t.start()
 
         while not self._thread_exit.is_set():
             ret, frame = self._cam.read()
@@ -156,19 +96,15 @@ class BarcodeScanner:
                 break
 
             if (i % 5) == 0:
-                find_barcode_queue.append(frame)
+                try:
+                    self._scan_queue.put_nowait(frame)
+                except queue.Full:
+                    self._scan_queue.get()
+                    self._scan_queue.put(frame)
             i += 1
 
-            try:
-                cur_contours = contour_queue.popleft()
-            except IndexError:
-                pass
-
-            for box in cur_contours:
-                cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
-
             data = cv2.imencode('.jpg', frame)[1].tostring()
-            for q in self._img_queues:
+            for q in copy.copy(self._img_queues):
                 try:
                     q.put_nowait(data)
                 except queue.Full:
