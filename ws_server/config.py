56b7147ec2adc0a13d0816448cd07ab669913c72
Q Misell <q@misell.cymru>
2019-05-15 14:53:40 +0100

    Config db interface for comparments

--- ws_server/config.py
+++ ws_server/config.py
@@ -1,4 +1,40 @@
 import dbm.gnu
+import struct
+import enum
+
+
+class CompartmentConfig:
+    @enum.unique
+    class CompartmentType(enum.Enum):
+        LOCAL = 0
+        CANBUS = 1
+
+    _FORMAT = "<BIH"
+
+    def __init__(self, comp_type: CompartmentType, channel: int, dev_id: int):
+        self._type = comp_type
+        self._channel = channel
+        self._dev_id = dev_id
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def channel(self):
+        return self._channel
+
+    @property
+    def dev_id(self):
+        return self._dev_id
+
+    @classmethod
+    def decode_config(cls, config: bytes):
+        if len(config) != struct.calcsize(cls._FORMAT):
+            return None
+        comp_type, channel, dev_id = struct.unpack(cls._FORMAT, config)
+        comp_type = cls.CompartmentType(comp_type)
+        return cls(comp_type, channel, dev_id)
 
 
 class Config:
@@ -17,6 +53,14 @@ class Config:
     @local_authority.setter
     def local_authority(self, val):
         self._db["local_authority"] = val
+
+    def get_compartment(self, comp_id: str):
+        val = self._db.get(f"comp_{comp_id}")
+        try:
+            val = val.decode()
+        except UnicodeDecodeError:
+            return None
+        return CompartmentConfig.decode_config(val)
 
     def __del__(self):
         self._db.close()
